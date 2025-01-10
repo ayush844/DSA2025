ad9fc4b34f3e23d1e073747e99b438a4bfa75d96
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2025-01-10 20:53:11 +0530

    Time: 4 ms (97.19%) | Memory: 44.9 MB (63.51%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,40 @@
+
+        class Pair{
+            int x, y;
+            public Pair(int x, int y){
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+
+        class MinStack {
+
+            Stack<Pair> st;
+
+            public MinStack() {
+                st = new Stack<>();
+            }
+
+            public void push(int val) {
+                int min;
+                if(st.isEmpty()){
+                    min = val;
+                }else{
+                    min = Math.min(st.peek().y, val);
+                }
+                st.push(new Pair(val, min));
+            }
+
+            public void pop() {
+                st.pop();
+            }
+
+            public int top() {
+                return st.peek().x;
+            }
+
+            public int getMin() {
+                return st.peek().y;
+            }
+        }
