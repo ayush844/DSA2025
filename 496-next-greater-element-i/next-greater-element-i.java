942e4b091119ca15152d279cf080cdcfcc1d8109
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2025-01-22 05:20:41 +0530

    Time: 3 ms (92.53%) | Memory: 43.5 MB (77.57%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,33 +1,22 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int nTotal = nums2.length;
-        int totalNGE[] = new int[nTotal];
-        int finalNGE[] = new int[n]; 
+        int[] ans = new int[nums1.length];
+
         Stack<Integer> st = new Stack<>();
-        for (int i = nTotal - 1; i >= 0; i--){
-            while (!st.isEmpty() && st.peek() <= nums2[i]){
-                st.pop();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums2){
+            while (!st.isEmpty() && num > st.peek()){
+                map.put(st.pop(), num);
             }
-            if (st.isEmpty()){
-                totalNGE[i] = -1;
-            }else {
-                totalNGE[i] = st.peek();
-            }
-            st.push(nums2[i]);
+            st.add(num);
         }
-        
-        for (int i = 0; i < n; i++){
-            int element = nums1[i];
-            for (int j = 0; j < nTotal; j++){
-                if (element==nums2[j]){
-                    finalNGE[i] = totalNGE[j];
-                    break;
-                }
-            }
+
+        int i = 0;
+        for (int num: nums1){
+            ans[i++] = map.getOrDefault(num, -1);
         }
-        
-        return finalNGE;
-        
-    }
+
+        return ans;
+    }    
 }
