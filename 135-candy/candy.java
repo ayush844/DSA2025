9b5940aa29a02feb4c27a489513b382b1aad4a3e
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2025-03-16 13:45:53 +0530

    Time: 3 ms (84.02%) | Memory: 46.1 MB (23.49%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,32 +1,32 @@
 class Solution {
     public int candy(int[] ratings) {
+int sum = 1;
+        int i = 1;
         int n = ratings.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-
-        left[0] = 1;
-        right[n-1] = 1;
-
-        for (int i = 1; i < n; i++){
-            if (ratings[i] > ratings[i-1]){
-                left[i] = left[i-1] + 1;
-            }else{
-                left[i] = 1;
+        while (i < n){
+            if (ratings[i] == ratings[i-1]){
+                sum = sum + 1;
+                i++;
+                continue;
             }
-        }
-        
-        for (int j = n - 2; j >= 0; j--){
-            if (ratings[j] > ratings[j + 1]){
-                right[j] = right[j + 1] + 1;
-            }else {
-                right[j] = 1;
+            int peak = 1;
+            while (i < n && ratings[i] > ratings[i-1]){
+                peak++;
+                sum += peak;
+                i++;
             }
-        }
-        
-        int sum = 0;
-        
-        for (int m = 0; m < n; m++){
-            sum = sum + Math.max(left[m], right[m]);    
+            int down = 1;
+            while (i < n && ratings[i] < ratings[i-1]){
+                sum += down;
+                down++;
+                i++;
+            }
+
+            if (down > peak){
+                sum += down - peak;
+            }
+
+            
         }
         
         return sum;
