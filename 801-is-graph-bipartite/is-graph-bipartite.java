a115bf18ccd955c55026fb3c3ea50a6973be4c44
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2025-07-29 08:17:09 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (39.59%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -0,0 +1,41 @@
+class Solution {
+
+
+    public boolean dfs4(int node, int col, int[] color, int[][] graph){
+        color[node] = col;
+
+
+        for (int it : graph[node]){
+            if(color[it] == -1){
+                if(dfs4(it, 1 - col, color, graph) == false){
+                    return false;
+                }
+            } else if (color[it] == col) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public boolean isBipartite(int[][] graph) {
+        int color[] = new int[graph.length];
+
+        for (int i = 0; i < graph.length; i++){
+            color[i] = -1;
+        }
+
+        for (int i = 0; i < graph.length; i++){
+            if (color[i] == -1){
+                if(dfs4(i, 0, color, graph) == false){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
+
+    }
+}
