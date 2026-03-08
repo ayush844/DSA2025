05fbd18d682b738fd216a9be851500ac445d4ee7
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2026-03-08 17:08:48 +0530

    Time: 6 ms (96.36%) | Memory: 44.7 MB (93.45%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -1,31 +1,23 @@
 class Solution {
     public String largestNumber(int[] nums) {
-        String[] asString = new String[nums.length];
+        String[] asStr = new String[nums.length];
 
         for(int i = 0; i < nums.length; i++){
-            asString[i] = String.valueOf(nums[i]);
+            asStr[i] = String.valueOf(nums[i]);
         }
 
-        // Arrays.sort(asString, new Comparator<String>(){
-        //     public int compare(String a, String b){
-        //         String order1 = a + b;
-        //         String order2 = b + a;
-        //         return order1.compareTo(order2);
-        //     }
-        // });
-        Arrays.sort(asString, (a,b) -> (b+a).compareTo(a+b));
+        Arrays.sort(asStr, (a, b) -> (b + a).compareTo(a + b));
 
-        if(asString[0].equals("0")){
+        if(asStr[0].equals("0")){
             return "0";
         }
 
         StringBuilder largestNum = new StringBuilder();
 
-        for(String numAsStr : asString){
+        for(String numAsStr : asStr){
             largestNum.append(numAsStr);
         }
 
         return largestNum.toString();
-
     }
 }
