890f7260c43abef81e9fe9db4fe04113c59e3062
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2026-03-28 17:49:45 +0530

    Time: 32 ms (40.82%) | Memory: 155.9 MB (18.66%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums == null || nums.length == 0 || k <= 0){
+            return new int[0];
+        }
+
+        int n = nums.length;
+        int[] result = new int[n-k+1];
+        Deque<Integer> dq = new LinkedList<>();
+
+        for(int i = 0; i < n; i++){
+            // remove indices that are out of our window
+            while(!dq.isEmpty() && (dq.peek() < (i - k + 1))){
+                dq.poll();
+            }
+            
+            // remove indices whose corresponding vlues are less than nums[i]
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
+                dq.pollLast();
+            }
+
+            // add current index to deque
+            dq.offer(i);
+
+            if(i >= k-1){
+                result[i-k+1] = nums[dq.peek()];
+            }
+        }
+
+        return result;
+
+    }
+}
