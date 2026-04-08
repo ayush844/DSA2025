0ed2e6b00f466ca4c37ef7150f020e955e7b46e9
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2026-04-08 21:55:00 +0530

    Time: 1 ms (77.18%) | Memory: 43.8 MB (51.12%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -14,43 +14,40 @@
  * }
  */
 class Solution {
- public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
 
-        if (root == null){
-            return result;
-        }
+        if(root == null) return ans;
 
-        Queue<TreeNode> nodesQueue = new LinkedList<>();
-        nodesQueue.add(root);
+        Deque<TreeNode> dq = new ArrayDeque<>();
+        dq.offer(root);
 
         boolean leftToRight = true;
 
-        while (!nodesQueue.isEmpty()) {
-            int size = nodesQueue.size();
-            List<Integer> row = new ArrayList<>();
+        while(!dq.isEmpty()){
+            int size = dq.size();
+            List<Integer> level = new ArrayList<>();
 
-            for (int i = 0; i < size; i++) {
-                TreeNode node = nodesQueue.poll();
-                row.add(node.val);  // Just add in normal order
+            for(int i = 0; i < size; i++){
+                if(leftToRight){
+                    TreeNode node = dq.pollFirst();
+                    level.add(node.val);
 
-                if (node.left != null){
-                    nodesQueue.add(node.left);
-                }
+                    if(node.left != null) dq.offerLast(node.left);
+                    if(node.right != null) dq.offerLast(node.right);
+                }else{
+                    TreeNode node = dq.pollLast();
+                    level.add(node.val);
 
-                if (node.right != null){
-                    nodesQueue.add(node.right);
+                    if(node.right != null) dq.offerFirst(node.right);
+                    if(node.left != null) dq.offerFirst(node.left);
                 }
             }
-
-            if (!leftToRight) {
-                Collections.reverse(row);  // Reverse for zigzag effect
-            }
-
-            result.add(row);
             leftToRight = !leftToRight;
+            ans.add(level);
         }
 
-        return result;
+        return ans;
+
     }
 }
