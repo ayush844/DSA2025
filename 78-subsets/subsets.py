acc7f3d235d6fe2f433664462acfa685d1021453
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2026-08-10 07:02:12 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (67.35%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,11 +1,23 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        outer = [[]]
-        for num in nums:
-            n = len(outer)
-            for i in range(n):
-                inner = outer[i] + [num]
-                outer.append(inner)
-
-        return outer
         
+        res = []
+        subset = []
+
+        def helper(index):
+            if index == len(nums):
+                res.append(subset[:])
+                return
+            # exclude
+            helper(index+1)
+
+            # include
+            subset.append(nums[index])
+            helper(index + 1)
+
+            # backtrack our changes
+            subset.pop()
+
+
+        helper(0)
+        return res
