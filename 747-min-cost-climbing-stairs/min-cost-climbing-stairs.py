4fdd8cf1695fe357f1599767e06b1a66e9adf5ba
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2026-08-16 22:31:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (47.08%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -2,23 +2,12 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n = len(cost)
 
-        minCost = [-1]*n
-
-        def helper(index):
-
-            if index > n-1:
-                return 0
-            
-            if minCost[index] != -1:
-                return minCost[index]
-
-            oneStep = cost[index] + helper(index+1)
-            twoStep = cost[index] + helper(index+2)
-
-            minCost[index] = min(oneStep, twoStep)
-
-            return minCost[index]
-
-
-        return min(helper(0), helper(1))
+        minCost = [0]*(n+1)
         
+        for i in range(2, n+1):
+            oneStep = minCost[i-1] + cost[i-1]
+            twoStep = minCost[i-2] + cost[i-2]
+
+            minCost[i] = min(oneStep, twoStep)
+
+        return minCost[n]
