40468032360f2a370a95272af80d0fd68d16b245
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2026-08-20 08:18:29 +0530

    Time: 47 ms (63.27%) | Memory: 22.6 MB (64.76%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -3,27 +3,24 @@ class Solution:
         n = len(word1)
         m = len(word2)
 
-        memo = {}
+        dp = [[0]*(m+1) for _ in range(n+1)]
 
-        def helper(i, j):
-            if i == n:
-                return m-j
-            if j == m:
-                return n-i
-
-            if (i, j) in memo:
-                return memo[(i, j)]
-
-            if word1[i] == word2[j]:
-                memo[(i, j)] = helper(i+1, j+1)
-                return memo[(i, j)]
-
-            insert = helper(i, j+1)
-            delete = helper(i+1, j)
-            replace = helper(i+1, j+1)
-
-            memo[(i, j)] = 1+min(insert, delete, replace)
-            return memo[(i, j)]
-
-        return helper(0, 0)
+        for i in range(n+1):
+            dp[i][0] = i
         
+        for j in range(m+1):
+            dp[0][j] = j
+
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    insert = dp[i][j-1]
+                    delete = dp[i-1][j]
+                    replace = dp[i-1][j-1]
+
+                    dp[i][j] = 1+min(insert, delete, replace)
+
+        return dp[n][m]
+                
