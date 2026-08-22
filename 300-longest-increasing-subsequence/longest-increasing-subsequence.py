f8cfe294f2cfbfedf5715c365171973b937fada5
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2026-08-22 13:19:17 +0530

    Time: 3900 ms (5.26%) | Memory: 147.4 MB (5.28%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -2,25 +2,15 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
 
-        dp = [[-1]*(n+1) for i in range(n+1)]
+        dp = [[0]*(n+1) for i in range(n+1)]
 
-        def helper(prev, curr):
+        for i in range(n-1, -1, -1):
+            for j in range(i, -1, -1):
+                exclude = dp[i+1][j]
+                include = 0
+                if j-1 == -1 or nums[i] > nums[j-1]:
+                    include = 1 + dp[i+1][i+1]
+                dp[i][j] = max(include, exclude)
 
-            if curr == n:
-                return 0
-
-            if dp[curr][prev] != -1:
-                return dp[curr][prev]
-
-            exclude = helper(prev, curr+1)
-
-            include = 0
-
-            if nums[curr] > nums[prev] or prev == -1:
-                include = 1 + helper(curr, curr+1)
-
-            dp[curr][prev] = max(exclude, include)
-
-            return dp[curr][prev]
-
-        return helper(-1, 0)
+        return dp[0][0]
+            
