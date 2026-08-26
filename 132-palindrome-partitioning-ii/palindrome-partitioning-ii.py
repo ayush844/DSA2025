c1b32bda430baf3391ce489f1df05fb8dc7b7200
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2026-08-26 08:25:01 +0530

    Time: 755 ms (28.04%) | Memory: 50.7 MB (24.71%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
@@ -1,8 +1,7 @@
 class Solution:
     def minCut(self, s: str) -> int:
         n = len(s)
-
-        isPalindrome = [[0]*n for _ in range(n)]
+        isPalindrome = [[False]*n for _ in range(n)]
 
         for l in range(1, n+1):
             for i in range(n-l+1):
@@ -11,32 +10,17 @@ class Solution:
                     isPalindrome[i][j] = True
                 elif s[i] == s[j] and (j == i+1 or isPalindrome[i+1][j-1]):
                     isPalindrome[i][j] = True
-                else:
-                    isPalindrome[i][j] = False
 
+        dp = [0]*n
 
-        dp = [[None]*n for _ in range(n)]
+        for end in range(n):
+            min_cuts = end
+            for start in range(end+1):
+                if isPalindrome[start][end]:
+                    if start == 0:
+                        min_cuts = 0
+                    else:
+                        min_cuts = min(min_cuts, dp[start-1] + 1)
+            dp[end] = min_cuts
 
-
-        memo = {}
-
-        def helper(start, end):
-            if start >= end or isPalindrome[start][end]:
-                return 0
-
-            if dp[start][end] != None:
-                return dp[start][end]
-
-            minimum = end - start
-
-            for end_index in range(start, end):
-                if isPalindrome[start][end_index]:
-                    minimum = min(
-                        minimum,
-                        1 + helper(end_index + 1, end)
-                    )
-
-            dp[start][end] = minimum
-            return minimum
-
-        return helper(0, n - 1)
+        return dp[n-1]
