adafe807bfcb761d205f02468aace621cc2c66a9
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2026-09-03 09:00:08 +0530

    Time: 2 ms (22.63%) | Memory: 19.3 MB (72.73%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,20 +1,21 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        sub = []
+        curr = []
         res = []
         arr = [i+1 for i in range(9)]
 
-        def helper(index, target):
-            if len(sub) == k and target == 0:
-                res.append(sub[:])
+        def helper(index, remaining):
+            if len(curr) == k and remaining == 0:
+                res.append(curr[:])
                 return
-            if len(sub) >= k:
+
+            if index>=9 or remaining < 0:
                 return
 
             for i in range(index, len(arr)):
-                sub.append(arr[i])
-                helper(i+1, target - arr[i])
-                sub.pop()
+                curr.append(arr[i])
+                helper(i+1, remaining-arr[i])
+                curr.pop()
 
         helper(0, n)
 
