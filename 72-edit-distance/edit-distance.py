82640e0e02a7497f56feb82b5766baaa2ad68d59
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2026-09-05 15:33:02 +0530

    Time: 31 ms (89.81%) | Memory: 22.5 MB (83.62%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -3,24 +3,27 @@ class Solution:
         n = len(word1)
         m = len(word2)
 
-        dp = [[0]*(m+1) for _ in range(n+1)]
+        dp = [[-1]*(m) for _ in range(n)]
 
-        for i in range(n+1):
-            dp[i][0] = i
-        
-        for j in range(m+1):
-            dp[0][j] = j
+        def helper(i, j):
+            if i >=n and j >= m:
+                return 0
+            if i >= n:
+                return m-j
+            if j >= m:
+                return n-i
 
-        for i in range(1, n+1):
-            for j in range(1, m+1):
-                if word1[i-1] == word2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]
-                else:
-                    insert = dp[i][j-1]
-                    delete = dp[i-1][j]
-                    replace = dp[i-1][j-1]
+            if dp[i][j] != -1:
+                return dp[i][j]
 
-                    dp[i][j] = 1+min(insert, delete, replace)
+            if word1[i] == word2[j]:
+                dp[i][j] = helper(i+1, j+1)
+            else:
+                insert = 1+helper(i, j+1)
+                delete = 1+helper(i+1, j)
+                replace = 1+helper(i+1, j+1)
+                dp[i][j] = min(insert, delete, replace)
 
-        return dp[n][m]
-                
+            return dp[i][j]
+
+        return helper(0, 0)
