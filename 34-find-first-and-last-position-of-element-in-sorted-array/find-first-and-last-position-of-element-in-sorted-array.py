79e467681dce62ff36cee0dd713d9d3c3cd9f2ca
Ayush Sharma <113158635+ayush844@users.noreply.github.com>
2026-09-16 07:40:50 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (61.82%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,36 @@
+class Solution:
+    def searchRange(self, nums: list[int], target: int) -> list[int]:
+        n = len(nums)
+        def firstOccurence(nums, target):
+            low = 0
+            high = n-1
+            first = -1
+
+            while low <= high:
+                mid = (low + high)//2
+                if nums[mid] == target:
+                    first = mid
+                    high = mid - 1
+                elif nums[mid] < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            return first
+
+        def lastOccurence(nums, target):
+            low = 0
+            high = n-1
+            last = -1
+
+            while low <= high:
+                mid = (low + high)//2
+                if nums[mid] == target:
+                    last = mid
+                    low = mid+1
+                elif nums[mid] < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            return last
+
+        return [firstOccurence(nums, target), lastOccurence(nums, target)]
